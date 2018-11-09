1281053d4fa932b6b8b54bad240d86220afc43b1
Marissa Faulkner <marissagrace808@gmail.com>
2018-11-09 13:29:21 -0800

    add start page function, displays all to dos

--- public/index.js
+++ public/index.js
@@ -9,34 +9,94 @@ function getToDosFromApi() {
 // 		return data
 // 	})
 
+
+//mentor example
+  // fetch('/todolist')
+  //   .then(response => response.json())
+  //   .then(responseJson => {
+  //   	console.log(responseJson)
+  //   	responseJson.forEach(toDo => {
+  //   		html.push(renderToDoItems(toDo))
+  //   		console.log(renderToDoItems(toDo))
+  //   	})
+  //   	return html
+  //   })
+  //   .then(html => {
+  //   	console.log(html)
+  //   	displayToDoItems(html)
+  //   })
+
+
   fetch('/todolist')
-    .then(response => response.json())
-    .then(responseJson => {
-    	console.log(responseJson)
-    	responseJson.forEach(toDo => {
-    		html.push(renderToDoItems(toDo))
-    		console.log(renderToDoItems(toDo))
-    	})
-    	return html
-    })
-    .then(html => {
-    	console.log(html)
-    	displayToDoItems(html)
-    })
+  	.then(response => response.json())
+  	.then(responseJson => {
+  		console.log(responseJson)
+  		filterToDoItems(responseJson)
+  	})
 }
 
+
+function filterToDoItems(data) {
+	var dailyToDos = [];
+	var weeklyToDos = [];
+	var monthlyToDos = [];
+	if (data) {
+		data.forEach(item => {
+			if (item.importance === 'daily') {
+				dailyToDos.push(item);
+			} 
+			if (item.importance === 'weekly') {
+				weeklyToDos.push(item);
+			}
+			if (item.importance === 'monthly') {
+				monthlyToDos.push(item);
+			}
+
+		});
+	};
+	displayDailyToDoItems(dailyToDos);
+	displayWeeklyToDoItems(weeklyToDos);
+	displayMonthlyToDoItems(monthlyToDos);
+}
 
 
 function renderToDoItems(data) {
 	return `
-	<p>${data.item}</p>
-	<p>${data.importance}</p>
+		<li><input type="checkbox" id="myCheck">${data.item} <span class="details">| ${data.hours} | ${data.dueDate}</span></li>
 	`;
 }
 
-function displayToDoItems(arr) {
-	console.log(arr.join(''))
-	$('.test').html(arr.join(''))
+function displayDailyToDoItems(arr) {
+	let dailys = [];
+	console.log(arr)
+	arr.forEach(toDo => {
+		dailys.push(renderToDoItems(toDo))
+	})
+	
+    $('.js-daily-list').html(dailys)
+
+}
+
+function displayWeeklyToDoItems(arr) {
+	let weeklys = [];
+	console.log(arr)
+	arr.forEach(toDo => {
+		weeklys.push(renderToDoItems(toDo))
+	})
+	
+    $('.js-weekly-list').html(weeklys)
+
+}
+
+function displayMonthlyToDoItems(arr) {
+	let monthlys = [];
+	console.log(arr)
+	arr.forEach(toDo => {
+		monthlys.push(renderToDoItems(toDo))
+	})
+	
+    $('.js-monthly-list').html(monthlys)
+
 
 }
 
@@ -56,25 +116,9 @@ function displayToDoItems(arr) {
 
 // 		}
 
-
-
-
-function renderDailyFilter(data) {
-	return `
-	<p><p>
-	`;
-}
-
-function displayDailyFilter(data) {
-	const dailyList = $('.js-daily-list')
-
-	dailyList
-		.html(renderDailyFilter(data));
+function startPage() {
+	getToDosFromApi();
 }
 
 
-
-
-$(function() {
-	getToDosFromApi()
-});
+$(startPage);
