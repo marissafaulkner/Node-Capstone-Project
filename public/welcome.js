da4a5c7080b6db0a734ebccad254180c2228ef88
Marissa Faulkner <marissagrace808@gmail.com>
2018-11-19 17:31:42 -0800

    feature complete, welcome intro page

--- public/welcome.js
+++ public/welcome.js
@@ -26,14 +26,41 @@ function firstNext() {
 	$('.js-welcome').on('click', '.js-next', function() {
 		$('.js-welcome').html(`
 			<h2>Now,</h2>
-			<p>pick a task that triggers that</p>
+			<p>pick that one "Very Important Task" that triggers that familiar feeling of wanting to procrastinate it away and send it to the top of your to do list.</p>
+			<p>Here's the trick, give in to your inclination to procrastinate, by starting on other tasks that need your attention.</p>
+			<p>Viola! You are now a contributing member of society!</p>
+
+			<button type="button" class="js-next-second">But what about the "Very Important Task"?</button>
 			`)
 	})
 }
 
+
+function secondNext() {
+	$('.js-welcome').on('click', '.js-next-second', function() {
+		$('.js-welcome').html(`
+			<h2>This part requires a bit of self-deception,</h2>
+			<p>Rank projects that seem significant yet have more flexible deadlines at the top to make that one Very Important Task an easier choice.</p>
+
+			<p>Now that you have all the info, lets get procrastinating!</p>
+			<button type="button" value="create page" class="js-next-final">Lets Go!</button>
+
+			`)
+	})
+}
+
+function finalNext() {
+	$('.js-welcome').on('click', '.js-next-final', function() {
+		window.location.href = "main.html";
+	})
+}
+
+
 function welcome() {
 	heresHow();
 	firstNext();
+	secondNext();
+	finalNext();
 }
 
 $(welcome);
